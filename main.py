648b8b1fab0ac943013982fc0a24652a2fca7f9d
Bernhard Schwarz <schwarz@users.noreply.github.com>
2018-02-06 15:49:16 +0100

    Improve logging

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ import praw
 import os
 
 def main():
+    log = logging.getLogger(__name__)
+    log.setLevel(logging.INFO)
+    formatter = logging.Formatter('{asctime} - {name} - [{levelname:8s}] {message}', style='{')
+    log.setFormatter(formatter)
     load_dotenv(find_dotenv())
     client_id = os.getenv('VIP_CLIENT_ID')
     client_secret = os.getenv('VIP_CLIENT_SECRET')
@@ -18,27 +22,27 @@ def main():
     vips = os.getenv('VIP_VIPS').split(',')
     subreddit = os.getenv('VIP_SUBREDDIT')
     reddit = praw.Reddit(client_id=client_id, client_secret=client_secret,
-                    password=password, user_agent="reddit-vip-flairs",
+                    password=password, user_agent='reddit-vip-flairs',
                     username=username)
 
     for comment in reddit.subreddit(subreddit).stream.comments():
         submission = comment.submission
         if (submission.author in vips and
            (not submission.link_flair_text or flair_text not in submission.link_flair_text)):
+            log.debug('found comment {} by u/{} on {}'.format(comment.id, comment.author, submission.id))
+            new_text = flair_text
+            # Append if flair text already set
+            if submission.link_flair_text:
+                new_text = "{}{}{}".format(
+                        submission.link_flair_text,
+                        flair_text_sep, flair_text)
             try:
-                new_text = flair_text
-                # Append if flair text already set
-                if submission.link_flair_text:
-                    new_text = "{}{}{}".format(
-                            submission.link_flair_text,
-                            flair_text_sep, flair_text)
-
                 data = {'link': submission.fullname, 'text': new_text,
                         'css_class': submission.link_flair_css_class}
-                reddit.post("/r/{}/api/flair".format(subreddit), data)
-                logging.info('assigned flair to submission {} triggered by comment {}'.format(comment.link_id, comment.id))
+                reddit.post('/r/{}/api/flair'.format(subreddit), data)
+                log.info('assigned flair to submission {} triggered by comment {}'.format(comment.link_id, comment.id))
             except:
-                logging.error('flair update triggered by comment {} failed'.format(comment.id))
+                log.error('flair update triggered by comment {} failed'.format(comment.id))
 
 if __name__ == "__main__":
     main()
